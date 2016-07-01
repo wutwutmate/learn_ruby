883d67d33703483f412647e23a47657b629a43fd
wutwutmate <dfrown1234@yahoo.com>
2016-06-30 17:03:16 -0700

    Added test to pig latin

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -68,5 +68,8 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
-
+  it "keeps words capitalized" do
+    s = translate("Harry lives in Oakland")
+    expect(s).to eq("Arryhay iveslay inay Oaklanday")
+  end
 end
