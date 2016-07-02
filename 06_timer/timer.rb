5e7bb88db6d80db04b3d3d8ae4ed8343b0fcde95
wutwutmate <dfrown1234@yahoo.com>
2016-07-02 15:24:23 -0700

    Finished final test

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,33 @@
 class Timer
-  #write your code here
+def seconds
+@seconds
+end
+def initialize
+@seconds = 0
+end
+def seconds=(time)
+@seconds = time
+end
+def time_string
+hour_divisor = 3600
+minutes_divisor = 60    
+hours = (@seconds/hour_divisor).floor
+if hours.to_s.length === 1
+clock = "0" +  hours.to_s + ":"
+else
+clock = hours.to_s + ":"
+end
+minutes = ((@seconds - (hours * hour_divisor))/minutes_divisor).floor
+if minutes.to_s.length === 1
+clock << "0" + minutes.to_s + ":"
+else
+clock << minutes.to_s + ":"
+end
+seconds = (@seconds - ((hours * hour_divisor) + (minutes * minutes_divisor)))
+if seconds.to_s.length === 1
+clock << "0" + seconds.to_s
+else
+clock << seconds.to_s
+end
+end
 end
